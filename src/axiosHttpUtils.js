53cdb12830937b305c1ca1383f5cf0b386c5d435
Akin <akin.gan@oocl.com>
2019-07-25 11:38:23 +0800

    feat: add the axios get request for filter & add a filter from in the Home page for filtering the packages.

--- src/axiosHttpUtils.js
+++ src/axiosHttpUtils.js
@@ -6,11 +6,20 @@ axios.defaults.timeout = 5000;
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-async function getAllPackages () {
+async function getAllPackages() {
   let response = await axios.get('/packages');
   return response.data || [];
 }
 
+async function getPackagesByParams(params) {
+  let url = `/packages?1=1`;
+  url += params.status !== '' ? `&status=${params.status}` : '';
+  url += params.hasOrdered !== '' ? `&hasOrdered=${params.hasOrdered}` : '';
+  let response = await axios.get(url);
+  return response.data || [];
+}
+
 export {
-  getAllPackages
+  getAllPackages,
+  getPackagesByParams
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,71 @@
 <template>
   <el-row class="home">
-    <el-table :data="allPackages" style="width: 100%">
-      <el-table-column type="selection" width="40" fixed></el-table-column>
-      <el-table-column label="运单号" sortable align="center">
-        <template slot-scope="scope">
-          <span>{{scope.row.logisticsNumber}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="客户姓名" sortable align="center">
-        <template slot-scope="scope">
-          <span>{{scope.row.customer.name}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="客户手机" sortable align="center">
-        <template slot-scope="scope">
-          <span>{{scope.row.customer.mobilePhone}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="状态" sortable align="center">
-        <template slot-scope="scope">
-          <span>{{scope.row.status === 1 ? '待取' : '已取'}}</span>
-        </template>
-      </el-table-column>
-
-    </el-table>
+    <el-row>
+      <el-form label-width="70px" label-position="left" :model="searchParams" ref="searchForm">
+        <el-col :span="4">
+          <el-form-item label="包裹状态">
+            <el-radio-group v-model="searchParams.status" size="small">
+              <el-radio label="1" class="radio-margin">已取件</el-radio>
+              <el-radio label="0" class="radio-margin">代取件</el-radio>
+              <el-radio label="" class="radio-margin">所有</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+        <el-col :span="1">&nbsp;</el-col>
+        <el-col :span="4">
+          <el-form-item label="预约状态">
+            <el-radio-group v-model="searchParams.hasOrdered" size="small">
+              <el-radio label="1" class="radio-margin">已预约</el-radio>
+              <el-radio label="0" class="radio-margin">未预约</el-radio>
+              <el-radio label="" class="radio-margin">所有</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary" size="small" icon="el-icon-search" class="search-btn" @click="filterPackages">搜索</el-button>
+        </el-col>
+      </el-form>
+    </el-row>
+    <el-row>
+      <el-table :data="allPackages" style="width: 100%">
+        <el-table-column type="selection" width="40" fixed></el-table-column>
+        <el-table-column label="运单号" sortable align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.logisticsNumber}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="客户姓名" sortable align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.customer.name}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="客户手机" sortable align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.customer.mobilePhone}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="状态" sortable align="center">
+          <template slot-scope="scope">
+            <span>{{scope.row.status === 1 ? '待取' : '已取'}}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
   </el-row>
 </template>
 
 <script>
-  import {getAllPackages} from "../axiosHttpUtils";
+  import {getAllPackages, getPackagesByParams} from "../axiosHttpUtils";
 
   export default {
     name: 'home',
     data() {
       return {
-        allPackages: []
+        allPackages: [],
+        searchParams: {
+          status: '',
+          hasOrdered: ''
+        }
       }
     },
     methods: {
@@ -47,8 +78,17 @@
         } catch (e) {
           console.log(e);
         }
+      },
+      async filterPackages () {
+        try {
+          let response = await getPackagesByParams(this.searchParams);
+          if (response) {
+            this.allPackages = response;
+          }
+        } catch (e) {
+          console.log(e);
+        }
       }
-
     },
     mounted() {
       this.getAllPackages();
